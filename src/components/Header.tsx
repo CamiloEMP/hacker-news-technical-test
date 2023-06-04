db793f2582358e299b3ec2503c69867bea4cb87a
b7c8p <morapaezc@gmail.com>
2023-06-03 19:14:48 -0500

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="p-4 bg-neutral-800">
+      <div className="flex items-center gap-2">
+        <img alt="" className="w-5 h-5" src="/logo.png" />
+        <h1 className="text-lg font-semibold">HACKER NEWS</h1>
+      </div>
+    </header>
+  )
+}
