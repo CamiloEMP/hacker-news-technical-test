2ec28c21d559f4d2d8763bd9d30bb24f239c08a7
b7c8p <morapaezc@gmail.com>
2023-06-05 19:12:37 -0500

    Adding styles to TopStories

--- src/components/TopStory.tsx
+++ src/components/TopStory.tsx
@@ -20,9 +20,17 @@ export function TopStory({ id }: { id: number }) {
   }
 
   return (
-    <Link to="/">
-      <h2>{story.title}</h2>
-      {story.score}
-    </Link>
+    <li className="flex flex-col justify-between w-full max-w-xs gap-4 p-4 border-2 rounded-md shadow border-neutral-800">
+      <a className="text-lg font-medium underline" href={story.url} rel="noopener" target="_blank">
+        {story.title}
+      </a>
+      <div className="flex items-center justify-between gap-2">
+        <small>{story.score} points</small>
+        <Link className="text-sm hover:underline" to="/">
+          {story.descendants} comments
+        </Link>
+        <small>by {story.by}</small>
+      </div>
+    </li>
   )
 }
--- src/pages/TopStories.tsx
+++ src/pages/TopStories.tsx
@@ -13,12 +13,13 @@ export default function TopStories() {
   }
 
   return (
-    <ul>
-      {topStories?.map(id => (
-        <li key={id}>
-          <TopStory id={id} />
-        </li>
-      ))}
-    </ul>
+    <section className="p-4 space-y-4">
+      <h2 className="text-xl font-semibold text-center">TOP STORIES</h2>
+      <ul className="flex flex-wrap justify-center gap-4">
+        {topStories?.map(id => (
+          <TopStory key={id} id={id} />
+        ))}
+      </ul>
+    </section>
   )
 }
